1fc7edb2aa361a6d21cd67be783f56c1650ab628
Matt Darlington <matthewsdarlington@gmail.com>
2023-07-30 18:29:59 -0600

    Assign integers to variables

--- variables.rb
+++ variables.rb
@@ -1,5 +1,14 @@
+# Assigned strings
 first_name = "Matt"
 last_name = "Darlington"
 current_session = "Mod0"
 program = "Back End"
+
+# Assigned integers
+my_age = 47
+my_birth_year = 1976
+my_weight = 185
+my_height = 74
+
+
 
