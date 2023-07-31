cdc6cc1160a9627154fda7ad5bb1309254d46f73
Matt Darlington <matthewsdarlington@gmail.com>
2023-07-30 18:42:52 -0600

    Reassign string variables, write put statements

--- variables.rb
+++ variables.rb
@@ -4,19 +4,33 @@ last_name = "Darlington"
 current_session = "Mod0"
 program = "Back End"
 
+first_name = "George"
+last_name = "Clooney"
+current_session = "Mod1"
+program = "Front End"
+
+
 # Assigned integers
 my_age = 47
 my_birth_year = 1976
 my_weight = 185
 my_height = 74
 
+
 # Assigned floats
 deposit = 1200.00
 my_body_temp = 98.6
 my_fav_station = 105.5
 
+
 # Assigned boolians
 my_balance_paid = true
 am_i_hungry = true
 am_i_tired = true
-am_i_cold = false
+am_i_hot = true
+
+
+puts "My first name is #{first_name}."
+puts "My last name is #{last_name}."
+puts "The initial deposit for Turing School is $#{deposit} dollars"
+puts "I heard you were feeling hot? That is #{am_i_hot}, these record-setting temperatures are making me hot."
